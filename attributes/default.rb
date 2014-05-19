69f9df669632331da3eecf8bb64bad33d8d873ee
Donavan Pantke <dpantke@appriss.com>
2014-05-18 22:23:25 -0400

    Fix type and switch to proper path name.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default[:s3fs][:install_tmp_path] = "/var/tmp/"
+default[:s3fs][:install_tmp_dir] = "/var/tmp/"
 default[:s3fs][:prefix] = "/usr/local"
 default[:s3fs][:version] = "1.7.1"
 default[:s3fs][:archive] = "s3fs-#{node[:s3fs][:version]}.tar.gz"
 default[:s3fs][:url] = "https://s3fs.googlecode.com/files/#{node[:s3fs][:archive]}"
-default[:s2fs][:patches] = nil
+default[:s3fs][:patches] = nil
